d9acbeca77a955f0b0da48b48156b1c0c66aa07e
NikSi039 <sidorova9999@internet.ru>
2024-01-18 22:17:37 +0200

    lesson7

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Write("введите число n:");
+int n=int.Parse(Console.ReadLine());
+int a =n/1000;
+int b =n%1000/100;
+int c= n%100/10;
+int d=n%10;
+Console.WriteLine ($"Сумма цифр:{a+b+c+d}");
+Console.WriteLine ($"Произведение цифр:{a*b*c*d}");
